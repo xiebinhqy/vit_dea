247ffd2b4999cc99d88a77f409ad91aea25e24ba
xiebinhqy <xiebin19900104@gmail.com>
2024-10-10 11:17:42 +0800

    update ys-test1

--- dev/.vitepress/config.mjs
+++ dev/.vitepress/config.mjs
@@ -5,9 +5,9 @@ import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
 export default defineConfig({
   title: "一武智能辅助文档",
   description: "A VitePress Site",
-  head: [["link", { rel: "icon", href: "/vit_dea/logo.png" }]],
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
   themeConfig: {
-    base: "/vit_dea/",
+    base: "/",
     outlineTitle:"目录",
     outline:[2,6],
     logo:'/logo.png',
